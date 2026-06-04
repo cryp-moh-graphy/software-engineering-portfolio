63365c678cf3c1e413e5cc4668a71c82e2ff18f2
MOH <icontact.mohammed@gmail.com>
2026-06-04 17:19:59 -0400

    Create Compare.cpp

---
+++ Inventra: Inventory System/Compare.cpp
@@ -0,0 +1,27 @@
+#include "Compare.hpp"
+#include <cmath>
+
+bool CompareItemWeight::lessThan(const Item& a, const Item& b) {
+    return a.weight_ < b.weight_;
+}
+
+bool CompareItemWeight::equal(const Item& a, const Item& b) {
+    return std::abs(a.weight_ - b.weight_) < 0.00001; // Note the threshold for float equality (we can't just use
+                                                      // '==')
+}
+
+bool CompareItemWeight::leq(const Item& a, const Item& b) {
+    return lessThan(a, b) || equal(a, b);
+}
+
+bool CompareItemType::lessThan(const Item& a, const Item& b) {
+    return a.type_ < b.type_;
+}
+
+bool CompareItemType::equal(const Item& a, const Item& b) {
+    return a.type_ == b.type_;
+}
+
+bool CompareItemType::leq(const Item& a, const Item& b) {
+    return lessThan(a, b) || equal(a, b);
+}
